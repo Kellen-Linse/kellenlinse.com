760ebf8bc4d55e2c06d18905a7201cfb61a8543a
Kellen-Linse <kblinse06@gmail.com>
2022-04-27 19:08:47 -0700

    Add bg image -in progress

--- src/Home/HomeBody.jsx
+++ src/Home/HomeBody.jsx
@@ -1,4 +1,14 @@
 import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+import Image from '../images/circuit_board.jpg'
+
+const styles = {
+  imgPaper: {
+      backgroundImage: `url(${Image})`,
+      height: 500,
+  }
+};
 
 function HomeBody() {
   const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
@@ -6,12 +16,13 @@ function HomeBody() {
   return (
     <>
       <Offset id='top' />
-      <div id='about' style={{ height: 500 }}>
+      <Box id='about' elevation={0} style={styles.imgPaper}>
+      <div style={{color: 'white'}} >
         <h1>This is About section</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
-        repellendus. Totam nihil similique a repellat minus dolor amet quasi.
-        Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+        To Do: resize the background image...
       </div>
+      </ Box>
+
       <div id='projects' style={{ height: 500 }}>
         <h1>This is Projects section</h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
