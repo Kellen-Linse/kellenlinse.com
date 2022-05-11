b14cb3f69ca6f318ae20e04748a6571ba4be02cf
Kellen-Linse <kblinse06@gmail.com>
2022-05-11 16:54:05 -0700

    WIP card and carosel

--- src/Pages/Home/Components/HomeBody/Projects/Components/ProjectCarousel.jsx
+++ src/Pages/Home/Components/HomeBody/Projects/Components/ProjectCarousel.jsx
@@ -1,46 +1,55 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 import {
-    Button,
-    Link,
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActions,
+  Stack,
 } from '@mui/material';
 
 export default function ProjectCarousel({ projects }) {
-    return (
-        <Carousel
-        duration={3000}
-        interval={10000}
-        indicators={false}
-        height={350}
-        animation='slide'
-        >
-        {projects.map((project, i) => (
-            <Item key={i} project={project} />
-        ))}
-        </Carousel>
-    );
+  return (
+    <Carousel
+      duration={3000}
+      interval={10000}
+      navButtonsAlwaysVisible
+      animation='slide'
+    >
+      {projects.map((project, i) => (
+        <Item key={i} project={project} />
+      ))}
+    </Carousel>
+  );
 }
 
 function Item({ project }) {
-    return (
-        <Card sx={{border: '2px solid red', height: 300}}>
-        <Typography variant='h6'>{project.title}</Typography>
+  return (
+    <Card elevation={0} sx={{ height: 600 }}>
+      <Stack height={'100%'} direction='column'>
+        <Typography variant='h5' sx={{ my: { xs: 1, md: 3 } }}>
+          {project.title}
+        </Typography>
         <CardMedia
-            component='img'
-            height='140'
-            image={project.img}
-            alt={project.img}
+          component='img'
+          image={project.img}
+          alt={project.img}
+          sx={{ px: { xs: 0, md: 12 }, height: { xs: '50%', md: '60%' } }}
         />
+
         <CardContent>
-            <Typography>{project.description}</Typography>
+          <Typography sx={{ p: { xs: 0, md: 3 } }}>
+            {project.description}
+          </Typography>
         </CardContent>
-        <Link href={project.link}>
-            <Button>Check it out!</Button>
-        </Link>
-        </Card>
-    );
+        <CardActions sx={{ justifyContent: 'center', p: 0, m: 0 }}>
+          <Button href={project.link} variant='outlined'>
+            Check it out!
+          </Button>
+        </CardActions>
+      </Stack>
+    </Card>
+  );
 }
--- src/Pages/Home/Components/HomeBody/Projects/Components/ProjectModal.jsx
+++ src/Pages/Home/Components/HomeBody/Projects/Components/ProjectModal.jsx
@@ -1,19 +1,16 @@
-import { Button, Grid, Paper, Box, Typography, Modal } from '@mui/material';
+import { Button, Stack, Box, Typography, Modal, Divider } from '@mui/material';
+import ProjectCarousel from './ProjectCarousel';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: {xs: '90%', lg: '50%'},
-  height: {xs: '80%', lg: '60%'},
+  width: { xs: '90%', lg: '50%' },
+  height: { xs: '90%', lg: '60%' },
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  pt: 1,
-  pb: 4,
-  pl: 4,
-  pr: 4,
   textAlign: 'center',
 };
 
@@ -21,31 +18,29 @@ export default function ProjectModal({ open, handleClose, card }) {
   return (
     <Modal open={open} onClose={handleClose} card={card}>
       <Box sx={style}>
-        <Grid container>
-          <Grid
-          container
-            direction='column'
-            justifyContent='space-between'
-            alignItems='center'
+        <Stack
+          height={'100%'}
+          direction='column'
+          justifyContent='space-around'
+          alignItems='stretch'
+        >
+       <Box >
+       <Typography
+            variant='h7'
+            component='h2'
+            sx={{color: 'primary.main', mb: 1 }}
           >
-            <Grid>
-
-            <Typography variant='h6' component='h2'>
-              {card.title}
-            </Typography>
-            </Grid>
-            <Grid>
-
-            <Paper sx={{ bgcolor: 'red', height: 200}}></Paper>
-            </Grid>
-            <Grid>
-
-            <Button variant='contained' onClick={() => handleClose()}>
-              Close
-            </Button>
-            </Grid>
-          </Grid>
-        </Grid>
+            {card.title}
+          </Typography>
+          <Divider />
+       </Box>
+          <ProjectCarousel projects={card.projects} />
+          <Box display='flex' justifyContent='flex-end'>
+          <Button sx={{width: 100, fontSize:'1.1em'}} onClick={() => handleClose()}>
+            Close
+          </Button>
+          </Box>
+        </Stack>
       </Box>
     </Modal>
   );
